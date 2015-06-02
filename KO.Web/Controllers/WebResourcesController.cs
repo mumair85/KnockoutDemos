0347d4f8ddf5b9b50fdc6952d7da9d2cf82bfb6b
M Umair Irshad <m_umair_85@hotmail.com>
2015-06-02 14:30:50 +0100

    Added web resources page for links to helpful sites

---
+++ KO.Web/Controllers/WebResourcesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KO.Web.Controllers
+{
+    public class WebResourcesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
